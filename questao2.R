27add70c91c12e4aec4f2ef470f19dde35b6b3a0
ruifernandees <dev.ruifernandes@gmail.com>
2021-11-20 16:28:02 -0300

    feat: questao 2
    Co-authored-by: antoinec8 <acfl@ic.ufal.br>

---
+++ questao2.R
@@ -0,0 +1,38 @@
+library(readxl)
+
+enem_db <- read_excel("/home/ruifernandees/Development/AT1-PEE/enem_db.xlsx")
+
+myTable <- table(enem_db$NO_MUNICIPIO_RESIDENCIA == "Boca da Mata")
+
+enem_bm <- enem_db[enem_db$NO_MUNICIPIO_PROVA == "Boca da Mata", ]
+
+table(enem_bm$NO_MUNICIPIO_RESIDENCIA)
+
+labs<-paste(c("Anadia = ", "Boca da Mata = ", "Limoeiro de Anadia = ", "Maceió = ", "Maribondo = ", "Taquarana = "),
+            round(table(enem_bm$NO_MUNICIPIO_RESIDENCIA)/length(enem_bm$NO_MUNICIPIO_RESIDENCIA) * 100, 
+                  digits=2), "%")
+
+pie(table(enem_bm$NO_MUNICIPIO_RESIDENCIA), labels=labs)
+
+nota_enem_quartil <- quantile(enem_bm$NOTA_ENEN, na.rm = TRUE)
+
+nota.cut <- cut(enem_bm$NOTA_ENEN,
+                breaks=nota_enem_quartil,
+                include.lowest = TRUE)
+
+idades <- enem_bm$NU_IDADE
+
+idade.cut <- cut(idades,
+                 breaks = c(min(idades), 16, 18, 24, 79), 
+                 include.lowest = TRUE)
+
+myTable <- table(idade.cut, nota.cut)
+
+barplot(myTable,
+        ylab="Freq. idade", 
+        col=c("blue", "red", "yellow", "green"),
+        xlab="Notas da Redação", 
+        main="Notas da Redação x Idade",
+        legend=TRUE,
+        beside=TRUE)
+    
