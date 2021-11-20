b126ed3d023ae25e33093da487ce19f5c47840b1
ruifernandees <dev.ruifernandes@gmail.com>
2021-11-20 11:51:21 -0300

    refactor: remove logs
    Co-authored-by: antoinec8 <acfl@ic.ufal.br>

--- questao1-1.R
+++ questao1-1.R
@@ -1,26 +1,13 @@
-# Boxplot
-#   Fa
-#   Fr
-#   Fac
-#   Frac
-
-# cumsum (F ac)
 library(readxl)
 
 enem_db <- read_excel("/home/ruifernandees/Development/AT1-PEE/enem_db.xlsx")
-View(enem_db)
-View(nrow(enem_db))
 
 nota_enem_quartil <- quantile(enem_db$NOTA_ENEN, na.rm = TRUE)
-View(nota_enem_quartil)
 
 Freq <- table(cut(enem_db$NOTA_ENEN, breaks = nota_enem_quartil, right=FALSE))
 FreqAc <- cumsum(Freq)
 FreqRel <- prop.table(Freq)
 FreqRelAc <- cumsum(FreqRel)
-
-View(FreqRelAc)
-View(FreqAc)
 
 TabResul <- cbind(Freq, FreqAc,
                   FreqRel = round(FreqRel*100, digits = 2),
