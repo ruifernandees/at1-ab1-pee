a7979fa8cda2e9e8cefa15e2bd336b7f8370fe8a
ruifernandees <dev.ruifernandes@gmail.com>
2021-11-20 13:52:36 -0300

    feat: questao1-3
    Co-authored-by: antoinec8 <acfl@ic.ufal.br>

---
+++ questao1-3.R
@@ -0,0 +1,18 @@
+library(readxl)
+
+enem_db <- read_excel("/home/ruifernandees/Development/AT1-PEE/enem_db.xlsx")
+
+nota_enem_quartil <- quantile(enem_db$NOTA_ENEN, na.rm = TRUE)
+
+nota.cut <- cut(enem_db$NOTA_ENEN,
+                breaks=nota_enem_quartil,
+                include.lowest = TRUE)
+
+myTable <- table(enem_db$TP_SEXO, nota.cut)
+
+barplot(myTable,
+        ylab="Freq. sexo", 
+        xlab="Quartis", 
+        main="Notas x Sexo",
+        legend=TRUE,
+        beside=TRUE)
